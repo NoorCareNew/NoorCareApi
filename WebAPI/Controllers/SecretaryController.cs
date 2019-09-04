e6bd6d310bb80a2632dea9f8a40881ac7b465766
addongroup <veerendrasingh34@gmail.com>
2019-09-04 21:56:20 +0530

    Update SecretaryController.cs
    secrataryId

--- WebAPI/Controllers/SecretaryController.cs
+++ WebAPI/Controllers/SecretaryController.cs
@@ -33,7 +33,7 @@ namespace WebAPI.Controllers
             return Request.CreateResponse(HttpStatusCode.Accepted, result);
         }
 
-        [Route("api/secretary/getdetail/{id}")]
+        [Route("api/secretary/getdetail/{secretaryId}")]
         [HttpGet]
         [AllowAnonymous]
         // GET: api/Secretary/5
