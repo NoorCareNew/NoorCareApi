39b039b9051bcd1bf3957ee3caa73b812092a217
addongroup <veerendrasingh34@gmail.com>
2019-09-18 20:05:32 +0530

    Get patient Details
    Get Patient Appointment details

--- WebAPI/Controllers/PatientController.cs
+++ WebAPI/Controllers/PatientController.cs
@@ -9,16 +9,46 @@ namespace WebAPI.Controllers
 {
     public class PatientController : ApiController
     {
-
+        IClientDetailRepository _clientDetailRepo = RepositoryFactory.Create<IClientDetailRepository>(ContextTypes.EntityFramework);
         IPatientPrescriptionRepository _prescriptionRepo = RepositoryFactory.Create<IPatientPrescriptionRepository>(ContextTypes.EntityFramework);
+        IMedicalInformationRepository _medicalInformationRepo = RepositoryFactory.Create<IMedicalInformationRepository>(ContextTypes.EntityFramework);
+        IInsuranceInformationRepository _insuranceInformationRepo = RepositoryFactory.Create<IInsuranceInformationRepository>(ContextTypes.EntityFramework);
 
-        [Route("api/patient/getall")]
+        [Route("api/patient/GetAllPatient")]
         [HttpGet]
         [AllowAnonymous]
-        public HttpResponseMessage GetAll()
+        public HttpResponseMessage GetAllPatient()
         {
-            var result = _prescriptionRepo.GetAll().ToList();
+            var result = _clientDetailRepo.GetAll().ToList();
             return Request.CreateResponse(HttpStatusCode.Accepted, result);
+        }
+
+        [Route("api/patient/GetPatient/{patientId}")]
+        [HttpGet]
+        [AllowAnonymous]
+        public HttpResponseMessage GetPatient(string patientId)
+        {
+            var result = _clientDetailRepo.Find(x => x.ClientId == patientId).FirstOrDefault();
+            return Request.CreateResponse(HttpStatusCode.Accepted, result);
+        }
+
+        [Route("api/patient/GetMedicalInformation/{patientId}")]
+        [HttpGet]
+        [AllowAnonymous]
+        public HttpResponseMessage GetMedicalInformation(string patientId)
+        {
+            var result = _medicalInformationRepo.Find(m => m.clientId == patientId).ToList();
+            return Request.CreateResponse(HttpStatusCode.Accepted, result);
+        }
+
+
+        [HttpGet]
+        [Route("api/patient/insuranceinfo/{clientId}")]
+        public IHttpActionResult getInsuranceInformation(string clientId)
+        {
+            var result= _insuranceInformationRepo.Find(x => x.ClientId == clientId).FirstOrDefault();
+
+            return Ok(result);
         }
 
         [Route("api/patient/getPrescription/{patientId}")]
