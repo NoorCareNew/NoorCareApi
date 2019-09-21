7971a2a5c253c523b7bae59e1736f147433d3ebb
addongroup <veerendrasingh34@gmail.com>
2019-09-22 00:52:09 +0530

    Remove Time from Upload Report
    Remove Time from Upload Report

--- WebAPI/Controllers/FileUploadController.cs
+++ WebAPI/Controllers/FileUploadController.cs
@@ -91,12 +91,12 @@ namespace WebAPI.Controllers
                     string day = DateTime.Now.Day.ToString();
                     string time = DateTime.Now.Hour.ToString() + DateTime.Now.Minute.ToString();// + DateTime.Now.Second.ToString();
 
-                    var filePath = HttpContext.Current.Server.MapPath("~/ClientDocument/" + desiesType + "/" + year + "/" + month + "/" + day + "/" + time);
-                    bool exists = System.IO.Directory.Exists(HttpContext.Current.Server.MapPath("~/ClientDocument/" + desiesType + "/" + year + "/" + month + "/" + day + "/" + time));
-                    if (exists)
-                    {
-                        File.Delete(filePath);
-                    }
+                    var filePath = HttpContext.Current.Server.MapPath("~/ClientDocument/" + desiesType + "/" + year + "/" + month + "/" + day);
+                   // bool exists = System.IO.Directory.Exists(HttpContext.Current.Server.MapPath("~/ClientDocument/" + desiesType + "/" + year + "/" + month + "/" + day));
+                    //if (exists)
+                    //{
+                    //    File.Delete(filePath);
+                    //}
                     Directory.CreateDirectory(filePath);
                     filePath = filePath + "/" + imageName;
                     postedFile.SaveAs(filePath);
